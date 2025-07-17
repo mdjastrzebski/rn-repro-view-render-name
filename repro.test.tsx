c28fd24d14f7d595e06e8f2a7fa98b3b48f5c168
Maciej Jastrzębski <mdjastrzebski@gmail.com>
2025-07-17 22:35:51 +0200

    repro scenario

---
+++ repro.test.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { View } from 'react-native';
+import { render, screen } from '@testing-library/react-native';
+
+test('repro invalid View render name', () => {
+  render(<View />);
+  expect(screen.toJSON()).toMatchInlineSnapshot(`<View_withRef />`);
+});
